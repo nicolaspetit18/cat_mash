72463ac1dcd912bdc569dc1db3fc542f0e2a45bc
Nicolas <nicolas.petit92330@gmail.com>
2018-01-29 07:08:17 +0100

    IMP# Add controller + setup API base behavior for mapper, injection and context

---
+++ CatMashApi/CatMashApi/Controllers/CatScoresController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using CatMashApi.Dto;
+using CatMashApi.Interfaces;
+using CatMashApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatMashApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CatScoresController : Controller
+    {
+        private ICatScoreService _catScoreService;
+        private IMapper _mapper;
+
+        public CatScoresController(
+            ICatScoreService catScoreService,
+            IMapper mapper)
+        {
+            _catScoreService = catScoreService;
+            _mapper = mapper;
+        }
+
+        // GET api/catscores
+        [HttpGet("GetTop")]
+        public IEnumerable<CatScoreDto> GetTop(int topMaxNumber)
+        {
+            var catScore = _catScoreService.GetTopScore(topMaxNumber);
+            return _mapper.Map<IEnumerable<CatScoreDto>>(catScore);
+        }
+
+        // GET api/catscores/5
+        [HttpGet]
+        public CatScoreDto Get(string id)
+        {
+            var catScore = _catScoreService.Get(id);
+            return _mapper.Map<CatScoreDto>(catScore);
+        }
+
+        // POST api/catscores
+        [HttpPost]
+        public void Post([FromBody]CatScoreDto catScoreDto)
+        {
+            var catScore = _mapper.Map<CatScore>(catScoreDto);
+            _catScoreService.Add(catScore);
+        }
+    }
+}
--- CatMashApi/CatMashApi/Startup.cs
+++ CatMashApi/CatMashApi/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CatMashApi.Helpers;
+using CatMashApi.Interfaces;
+using CatMashApi.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CatMashApi
 {
@@ -23,7 +21,23 @@ namespace CatMashApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //configure automapper for json api
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new AutoMapperProfile());
+            });
+            var mapper = config.CreateMapper();
+            services.AddSingleton(mapper);
+
+            //Configure mvc
             services.AddMvc();
+
+            //connect to mysql database
+            services.AddDbContext<CatScoreContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // configure Dynamic injection for application services
+            services.AddScoped<ICatScoreService, CatScoreService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
